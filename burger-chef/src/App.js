abe951da6274babb861c4aabfc91f3ba36717657
Babatunde Mattieu <bbmattieu9@gmail.com>
2019-09-18 13:23:15 +0100

    Created base layout structure for burger chef app

--- burger-chef/src/App.js
+++ burger-chef/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'; 
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
-      <div  >
-       
+      <div >
+        <Layout>
+            <p>Testing the layout component!</p>
+        </Layout>
       </div>
     );
   }
